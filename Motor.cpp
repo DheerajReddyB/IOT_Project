8c63cfdfb7eee8176d3e061b9f8bd55609b5b961
Saju K K <sajukk@ai-robots.co>
2024-04-23 00:13:17 +0530

    xdebug

--- Motor.cpp
+++ Motor.cpp
@@ -26,60 +26,6 @@ BLEService UltraSonicService("19b10000-e8f2-537e-4f6c-d104768a1215");
 
 
 
-/*#include <ArduinoBLE.h>
-
-//custom 128-bit UUID, read and writable by central
-BLEService MotorService("19b10000-e8f2-537e-4f6c-d104768a1214");
-BLEService UltraSonicService("19b10000-e8f2-537e-4f6c-d104768a1215");
-
-//setting read and write characteristics 
-//BLEByteCharacteristic switchCharacteristic("19b10000-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite);
-BLEIntCharacteristic switchCharacteristic("19b10000-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite);
-
-void setup() {
-  Serial.begin(9600);
- 
-  // motor controls
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(ENB,OUTPUT);
-
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,LOW);
-
-  analogWrite(ENA,Speed);
-  analogWrite(ENB,Speed);
-
-  
-  // BLE initialization
-  if (!BLE.begin()) { 
-    Serial.println("Starting Bluetooth® Low Energy module failed!");
-    while (1);
-  }
-
-  // set advertised local name and service UUID:
-  BLE.setDeviceName("IOT_Project");
-  BLE.setLocalName("IOT_Project");
-  BLE.setAdvertisedService(MotorService);
-
-  // add the characteristic to the service
-  MotorService.addCharacteristic(switchCharacteristic);
-
-  // add service
-  BLE.addService(MotorService);
-
-  // set the initial value for the characeristic:
-  switchCharacteristic.writeValue(0);
-
-  // start advertising
-  BLE.advertise();
-  Serial.println("BLE Peripheral");*/
-
 
 void DigitalWrite() {
   digitalWrite(IN1, LOW);
@@ -239,9 +185,5 @@ void Motor() {
         }
       }
     }
-    /*
-    // the central has disconnected
-    Serial.println("Disconnected from central: ");
-    */
   }
 }
